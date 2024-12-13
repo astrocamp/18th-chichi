b8fb71e0f8e4b85d61c9988280a91f92d5d5816a
JW.921 <a27371510@gmail.com>
2024-12-13 15:22:46 +0800

    refactor: removed projects column pending Projects FK

--- faqs/forms.py
+++ faqs/forms.py
@@ -4,4 +4,4 @@ from .models import Faq
 class FaqForm(ModelForm):
      class Meta:
           model = Faq
-          fields = ["project", "question", "answer"]
+          fields = ["question", "answer"]
