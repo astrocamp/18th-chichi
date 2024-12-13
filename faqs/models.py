0d21d27996be31643d573da8ab02b97ada87d661
JW.921 <a27371510@gmail.com>
2024-12-13 15:22:46 +0800

    refactor: removed projects column pending Projects FK

--- faqs/models.py
+++ faqs/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
 class Faq(models.Model):
-    project = models.CharField(max_length=100)
     question = models.CharField(max_length=100)
     answer = models.TextField(null=True)
