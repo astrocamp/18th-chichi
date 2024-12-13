f5fa09b888f8c2a3a8f2e68b448aa3f1caa95620
IanYang1106 <monoyen2001@gmail.com>
2024-12-13 10:24:04 +0800

    feat: add homepages and accounts system

---
+++ accounts/views.py
@@ -0,0 +1,49 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login as login_user, logout as logout_user
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.views.decorators.http import require_POST
+
+
+def login(request):
+    if request.POST:
+        # 驗證
+        user = authenticate(
+            username=request.POST.get("username"),
+            password=request.POST.get("password"),
+        )
+
+        if user is not None:
+            login_user(request, user)
+            messages.success(request, "登入成功")
+            return redirect("homepages:homepages")
+        else:
+            messages.success(request, "登入失敗")
+            return redirect("accounts:login")
+
+    return render(request, "accounts/login.html")
+
+
+def register(request):
+    if request.POST:
+        # 註冊
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "註冊成功")
+            return redirect("homepages:homepages")
+        else:
+            return HttpResponse(form.error_messages)
+
+    return render(request, "accounts/register.html")
+
+
+@require_POST
+@login_required
+def logout(request):
+    logout_user(request)
+    messages.success(request, "已登出")
+    return redirect("homepages:homepages")
--- core/settings.py
+++ core/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "projects"
+    "accounts",
+    "homepages",
 ]
 
 MIDDLEWARE = [
@@ -98,16 +99,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
