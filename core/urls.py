194f36d3716ef03fea0b203287a28f153c4c8371
LAPTOP-0GVE4AVI\Linda <lindaqaq88@gmail.com>
2024-12-16 17:44:21 +0800

    ci:resolve reflict

--- core/urls.py
+++ core/urls.py
@@ -3,14 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-<<<<<<< HEAD
     path("", include("homepages.urls")),
     path("accounts/", include("accounts.urls")),
-<<<<<<< HEAD
     path("projects/", include("projects.urls")),
     path("faqs/", include("faqs.urls")),
-=======
-=======
     path(
         "",
         include("homepages.urls"),
@@ -20,6 +16,5 @@ urlpatterns = [
         include("accounts.urls"),
     ),
     path("projects/", include("projects.urls")),
->>>>>>> 91d25ef (fix:setting/url/)
     path("users/", include("users.urls")),
 ]
