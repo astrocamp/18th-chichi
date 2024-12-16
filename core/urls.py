6345134a6abe95e73efa43fc250a7198f0fb917c
Allen <rainbluej@gmail.com>
2024-12-16 12:26:38 +0800

    feat: issue/5 Users CRUD

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,11 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("homepages.urls")),
     path("accounts/", include("accounts.urls")),
+<<<<<<< HEAD
     path("projects/", include("projects.urls")),
     path("faqs/", include("faqs.urls")),
+=======
+    path("users/", include("users.urls")),
+>>>>>>> 1b3fc01 (feat: issue/5 Users CRUD)
 ]
 
---
+++ users/forms.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm
+from .models import User
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            "name",
+            "phone",
+            "birthday",
+            "location",
+            "website",
+            "bio",
+        ]
