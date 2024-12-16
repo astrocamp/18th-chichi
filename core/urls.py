82285690d2d9c8bb288a9ef3a79ba8f5c3c43dbd
Allen <rainbluej@gmail.com>
2024-12-16 16:43:59 +0800

    fix:setting/url/

--- core/urls.py
+++ core/urls.py
@@ -3,13 +3,23 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+<<<<<<< HEAD
     path("", include("homepages.urls")),
     path("accounts/", include("accounts.urls")),
 <<<<<<< HEAD
     path("projects/", include("projects.urls")),
     path("faqs/", include("faqs.urls")),
 =======
+=======
+    path(
+        "",
+        include("homepages.urls"),
+    ),
+    path(
+        "accounts/",
+        include("accounts.urls"),
+    ),
+    path("projects/", include("projects.urls")),
+>>>>>>> 91d25ef (fix:setting/url/)
     path("users/", include("users.urls")),
->>>>>>> 1b3fc01 (feat: issue/5 Users CRUD)
 ]
-
