04cfdca3df1934f1c059465a8b264f9f2072bb88
Allen <rainbluej@gmail.com>
2024-12-17 20:07:23 +0800

    feat:upload function

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "users"
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path("<int:id>", views.show, name="show"),
     path("<int:id>/edit", views.edit, name="edit"),
     path("<int:id>/delete", views.delete, name="delete"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
