01668d284c63776ae96bc137f3ff6154a0af44c0
陳致宇 <chenchihyu@MacBook-Air.local>
2024-12-18 15:00:27 +0800

    fix: hotfix wrong login page

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("projects/", include("projects.urls")),
     path("faqs/", include("faqs.urls")),
     path("users/", include("users.urls")),
-    path("accounts/", include("allauth.urls")),
     path("accounts/", include("accounts.urls")),
+    path("accounts/", include("allauth.urls")),
     path('comments/', include("comments.urls")),
 ]
