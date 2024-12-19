8ad8550882416d44af514a5d4685a031b1c41d31
陳致宇 <chenchihyu@MacBook-Air.local>
2024-12-19 18:54:24 +0800

    feat: account one_to_many projects

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from projects.views import index,new
 
 app_name = "accounts"
 
@@ -7,4 +8,7 @@ urlpatterns = [
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
     path("register/", views.register, name="register"),
+    path("<int:id>", views.index, name="index"),
+    path("<int:id>/projects",index, name="projects"),
+    path("<int:id>/projects/new",new, name="projects_new")
 ]
--- projects/urls.py
+++ projects/urls.py
@@ -1,13 +1,12 @@
 
 from django.urls import path
-from .views import index,new,show,edit,delete
+from .views import show,edit,delete
 
 
 app_name = "projects"
 
 urlpatterns = [
-    path('', index , name = "index" ),
-    path('new/', new , name = "new" ),
+    # 移到account urls
     path('<int:id>', show , name = "show" ),
     path('<int:id>/edit', edit , name = "edit" ),
     path('<int:id>/delete', delete , name = "delete" ),
