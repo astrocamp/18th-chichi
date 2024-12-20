c9920c429c274f990038ffec8a5e21d3256d47a2
Jason Wang <a27371510@gmail.com>
2024-12-20 16:05:07 +0800

    2243q3t

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 from .views import index,new,show,edit,delete
-
+from faqs.views import index as faq_index, new as faq_new
 
 app_name = "projects"
 
@@ -11,7 +11,6 @@ urlpatterns = [
     path('<int:id>', show , name = "show" ),
     path('<int:id>/edit', edit , name = "edit" ),
     path('<int:id>/delete', delete , name = "delete" ),
-
-
-
+    path('<int:id>/faq', faq_index , name = "faq_index" ),
+    path('<int:id>/faq/new', faq_new , name = "faq_new" ),
 ]
