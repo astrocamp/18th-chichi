508d9dcce4773041e7a3ec312017282d7e728f98
IanYang1106 <monoyen2001@gmail.com>
2024-12-21 16:02:10 +0800

    feat: update FK relationship between update_records and project

--- update_records/models.py
+++ update_records/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from projects.models import Project
 
 class UpdateRecord(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(null=True)
+    project=models.ForeignKey(Project,on_delete=models.CASCADE,related_name="update_records")
--- update_records/urls.py
+++ update_records/urls.py
@@ -7,7 +7,7 @@ app_name = "update_records"
 urlpatterns = [
     path('', index , name = "index" ),
     path('new/', new , name = "new" ),
-    path('<int:id>', show , name = "show" ),
-    path('<int:id>/edit', edit , name = "edit" ),
-    path('<int:id>/delete', delete , name = "delete" ),
+    path('<int:update_id>', show , name = "show" ),
+    path('<int:update_id>/edit', edit , name = "edit" ),
+    path('<int:update_id>/delete', delete , name = "delete" ),
 ]
