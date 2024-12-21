bf2dbb74a0e9db795e9c352afad15dde0d7a8d92
陳致宇 <chenchihyu@MacBook-Air.local>
2024-12-21 22:48:50 +0800

    feat: account one_to_many projects

--- projects/urls.py
+++ projects/urls.py
@@ -1,13 +1,12 @@
 
 from django.urls import path
-from .views import index,new,show,edit,delete, collect_projects
+from .views import show,edit,delete, collect_projects
 
 
 app_name = "projects"
 
 urlpatterns = [
-    path('', index , name = "index" ),
-    path('new/', new , name = "new" ),
+    # 移到account urls
     path('<int:id>', show , name = "show" ),
     path('<int:id>/edit', edit , name = "edit" ),
     path('<int:id>/delete', delete , name = "delete" ),
