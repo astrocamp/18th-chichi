bfd714f3e9b723a6ea8e41bb2505547a2f4d41ce
LAPTOP-0GVE4AVI\Linda <lindaqaq88@gmail.com>
2024-12-23 12:28:11 +0800

    feat: account&comments_replies one to many collect

--- comments_replies/models.py
+++ comments_replies/models.py
@@ -8,7 +8,9 @@ class CommentsReplies(models.Model):
     content = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(null=True)
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    comment = models.ForeignKey(
+        Comment, on_delete=models.CASCADE, related_name="comments_replies"
+    )
     account = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="comments_replies"
     )
