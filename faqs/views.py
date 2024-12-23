a6cecda595bf6fbb8bab6b271d074e6ae0010325
Jason Wang <a27371510@gmail.com>
2024-12-23 10:39:12 +0800

    issues/30 feat: faqs to projects

--- faqs/views.py
+++ faqs/views.py
@@ -4,43 +4,51 @@ from django.utils import timezone
 
 from .forms import FaqForm
 from .models import Faq
+from projects.models import Project
 
-def index(request):
+def index(request, id):
+    project = get_object_or_404(Project, id=id)
     if request.POST:
         form = FaqForm(request.POST)
-        form.save()
-        messages.success(request, "新增成功")
-        return redirect("faqs:index")
+        if form.is_valid():
+            faq = form.save(commit=False)
+            faq.project = project
+            faq.save()
+            messages.success(request, "新增成功")
+            return redirect("projects:faq_index", id=id)
     
-    faqs = Faq.objects.order_by("-id")
-    return render(request, "faqs/index.html", {"faqs": faqs})
+    faqs = Faq.objects.filter(project=project)
+    return render(request, "faqs/index.html", {"faqs": faqs, "project": project}) 
 
-def new(request):
+def new(request, id):
+    project = get_object_or_404(Project, id=id)
     form = FaqForm()
-    return render(request, "faqs/new.html", {"form": form})
+    return render(request, "faqs/new.html", {"form": form, "project": project})
 
 def show(request, id):
     faq = get_object_or_404(Faq, id=id)
+    project = get_object_or_404(Project, id=faq.project.id)
     if request.POST:
         form = FaqForm(request.POST, instance=faq)
         form.save()
         faq.update_at = timezone.now()
         faq.save()
         messages.success(request, "更新成功")
-        return redirect("faqs:index")
+        return redirect("projects:faq_index" ,id=project.id)
     
-    return render(request, "faqs/show.html", {"faq": faq})
+    return render(request, "faqs/show.html", {"faq": faq,} )
 
 def edit(request, id):
     faq = get_object_or_404(Faq, id=id)
     form = FaqForm(instance=faq)    
-    return render(request, "faqs/edit.html", {"faq": faq, "form": form})
+    return render(request, "faqs/edit.html", {"faq": faq, "form": form, })
 
 def delete(request, id):
     faq = get_object_or_404(Faq, id=id)
+    project = get_object_or_404(Project, id=faq.project.id)
     if request.POST:
         faq.delete()
         messages.success(request, "刪除成功")
-        return redirect("faqs:index")
+        return redirect("projects:faq_index", id = project.id)
 
-    return render(request, "faqs/delete.html", {"faq": faq})
+    return render(request, "faqs/delete.html", {"faq": faq,})
