7400aedd1f59051fcafce6c006a015849d3299f8
viviennehsin <viviennehsin@gmail.com>
2024-12-23 21:41:25 +0800

    feat: categories and subcategories relationship completed

--- subcategories/urls.py
+++ subcategories/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
+<<<<<<< HEAD
 from .views import subindex
+=======
+from .views import index
+>>>>>>> 0e4551c (feat: categories and subcategories relationship completed)
 
 app_name = "subcategories"
 
 urlpatterns = [
+<<<<<<< HEAD
     path("", subindex, name="subindex"),
+=======
+    path("<int:category_id>/", index, name="index"),
+>>>>>>> 0e4551c (feat: categories and subcategories relationship completed)
 ]
