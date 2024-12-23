bc29a99d60987e2ce20c8ba6604ab3eceba9e6e2
viviennehsin <viviennehsin@gmail.com>
2024-12-23 21:41:25 +0800

    feat: categories and subcategories relationship completed

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,14 @@ urlpatterns = [
     path("comments/", include("comments.urls")),
     path("categories/", include("categories.urls")),
     path("rewards/", include("rewards.urls")),
+<<<<<<< HEAD
     path("subcategories/", include("subcategories.urls")),
     path("comments_replies/",include("comments_replies.urls")),
     path('update_records/', include("update_records.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+=======
+    path("comments_replies/", include("comments_replies.urls")),
+    path("update_records/", include("update_records.urls")),
+    path("subcategories/", include("subcategories.urls")),
+]
+>>>>>>> 0e4551c (feat: categories and subcategories relationship completed)
--- subcategories/models.py
+++ subcategories/models.py
@@ -1,8 +1,19 @@
 from django.db import models
+<<<<<<< HEAD
 
 
 class SubCategory(models.Model):
     title = models.CharField(max_length=20, unique=True)
+=======
+from categories.models import Category
+
+
+class SubCategory(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="subcategories"
+    )
+>>>>>>> 0e4551c (feat: categories and subcategories relationship completed)
 
     def __str__(self):
         return self.title
