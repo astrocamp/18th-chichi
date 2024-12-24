b50f581e31a1be451fc6821e4deb769df47c6625
LAPTOP-0GVE4AVI\Linda <lindaqaq88@gmail.com>
2024-12-24 22:53:34 +0800

    refactor: resolve projects conflicts

--- projects/models.py
+++ projects/models.py
@@ -2,47 +2,57 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import now
 
+
 class Project(models.Model):
     STATUS_CHOICES = [
-        ('pending', '待上架'),
-        ('live', '已上架'),
-        ('ended', '已下架'),
+        ("pending", "待上架"),
+        ("live", "已上架"),
+        ("ended", "已下架"),
     ]
     title = models.CharField(max_length=100)
-    subtitle = models.CharField(max_length=100,null=True)
-    cover_image = models.FileField(upload_to="uploads/" ,null=True)
-    raised_amount = models.DecimalField(null=True ,decimal_places= 0 , max_digits=10)
-    goal_amount = models.DecimalField(decimal_places= 0 , max_digits=10)
+    subtitle = models.CharField(max_length=100, null=True)
+    cover_image = models.FileField(upload_to="uploads/", null=True)
+    raised_amount = models.DecimalField(null=True, decimal_places=0, max_digits=10)
+    goal_amount = models.DecimalField(decimal_places=0, max_digits=10)
     start_at = models.DateTimeField(null=True)
     end_at = models.DateTimeField()
     story = models.TextField()
-    location = models.CharField(null=True,max_length=100)
+    location = models.CharField(null=True, max_length=100)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(null=True)
     account = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(
         max_length=10,
         choices=STATUS_CHOICES,
-        default='pending',
+        default="pending",
     )
 
     collect_account = models.ManyToManyField(
         User,
         related_name="collect_projects",
         through="CollectProject",
-        through_fields=("project","account"),
+        through_fields=("project", "account"),
     )
+
     def update_status(self):
         """
         更新專案狀態：根據時間設定自動上架或下架
         """
         current_time = now()
-        if self.status == 'pending' and self.start_at and current_time >= self.start_at:
-            self.status = 'live'
+        if self.status == "pending" and self.start_at and current_time >= self.start_at:
+            self.status = "live"
             self.save()
-        elif self.status == 'live' and self.end_at and current_time >= self.end_at:
-            self.status = 'ended'
+        elif self.status == "live" and self.end_at and current_time >= self.end_at:
+            self.status = "ended"
             self.save()
+
+    favorite_users = models.ManyToManyField(
+        User,
+        related_name="favorite_users",
+        through="FavoritePrject",
+        through_fields=("project", "account"),
+    )
+
 
 class CollectProject(models.Model):
     account = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -50,7 +60,7 @@ class CollectProject(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
 
 
-    
-
-    
-   
+class FavoritePrject(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    account = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import ProjectFrom
-from .models import Project, CollectProject
+from .models import Project, CollectProject, FavoritePrject
 from django.utils.timezone import localtime
 from django.views.decorators.http import require_POST
 from django.utils import timezone
@@ -10,15 +10,16 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 
 
+
 def index(request, id):
     account = get_object_or_404(User, id=id)
     if request.POST:
-        form = ProjectFrom(request.POST) 
+        form = ProjectFrom(request.POST)
         if form.is_valid():
             project = form.save(commit=False)
             project.account = account
             project.save()
-            return redirect("accounts:projects" , id = account.id)
+            return redirect("accounts:projects", id=account.id)
         else:
             return HttpResponse("輸入錯誤")
 
@@ -28,19 +29,19 @@ def index(request, id):
     return render(request, "projects/index.html", {"projects":projects,"account":account})
         
 
-def new(request,id):
+
+def new(request, id):
     account = get_object_or_404(User, id=id)
-    return render(request, "projects/new.html",{"account":account})
-
-
+    return render(request, "projects/new.html", {"account": account})
 
 
 def show(request, id):
-    project = get_object_or_404(Project,id=id)
+    project = get_object_or_404(Project, id=id)
     account = get_object_or_404(User, id=request.user.id)
 
 
     if request.POST:
+<<<<<<< HEAD
         # 處理上架邏輯
         if "publish" in request.POST:  # 檢查是否點擊了上架按鈕
             if project.status == 'pending':
@@ -71,42 +72,86 @@ def show(request, id):
 
     
     collected = CollectProject.objects.filter(account=request.user, project=project).first()
+=======
+        form = ProjectFrom(request.POST, instance=project)
+        form.save()
+        project.update_at = timezone.now()
+        project.save()
+        return redirect("projects:show", id=project.id)
+>>>>>>> d2f40ab (refactor: resolve projects conflicts)
 
-    return render(request, "projects/show.html",{"project":project,"collected":collected,"account":account})
+    collected = CollectProject.objects.filter(
+        account=request.user, project=project
+    ).first()
 
+    favorited = project.favorite_users.filter(id=request.user.id).first()
 
+    return render(
+        request,
+        "projects/show.html",
+        {
+            "project": project,
+            "collected": collected,
+            "account": account,
+            "favorited": favorited,
+        },
+    )
 
 
 def edit(request, id):
-    project = get_object_or_404(Project,id=id)
+    project = get_object_or_404(Project, id=id)
 
-    format_time_start =localtime(project.start_at).strftime('%Y-%m-%dT%H:%M')
-    format_time_end =localtime(project.end_at).strftime('%Y-%m-%dT%H:%M')
-    return render(request, "projects/edit.html",{"project":project,
-"format_time_start":format_time_start,"format_time_end":format_time_end})
+    format_time_start = localtime(project.start_at).strftime("%Y-%m-%dT%H:%M")
+    format_time_end = localtime(project.end_at).strftime("%Y-%m-%dT%H:%M")
+    return render(
+        request,
+        "projects/edit.html",
+        {
+            "project": project,
+            "format_time_start": format_time_start,
+            "format_time_end": format_time_end,
+        },
+    )
 
 
 def delete(request, id):
-    project = get_object_or_404(Project,id=id)
+    project = get_object_or_404(Project, id=id)
     account = get_object_or_404(User, id=request.user.id)
     if request.POST:
         project.delete()
 
-        return redirect("accounts:projects", id = account.id )
+        return redirect("accounts:projects", id=account.id)
 
-    return render(request, "projects/delete.html",)
+    return render(
+        request,
+        "projects/delete.html",
+    )
 
 
 @login_required
 @require_POST
 def collect_projects(request, id):
     project = get_object_or_404(Project, id=id)
-    collect, created  = CollectProject.objects.get_or_create(
-        account = request.user,
-        project = project,
+    collect, created = CollectProject.objects.get_or_create(
+        account=request.user,
+        project=project,
     )
 
     if not created:
         collect.delete()
+
+    return redirect("projects:show", id=project.id)
+
+
+@login_required
+@require_POST
+def like_projects(request, id):
+    project = get_object_or_404(Project, id=id)
+    favorite = project.favorite_users.filter(id=request.user.id).first()
+
+    if favorite:
+        FavoritePrject.objects.get(account=request.user, project=project).delete()
+    else:
+        FavoritePrject(account=request.user, project=project).save()
 
     return redirect("projects:show", id=project.id)
