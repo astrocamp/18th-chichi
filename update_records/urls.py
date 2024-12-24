83aa6ae649eda4bca7485202f2b97efa5dca1331
IanYang1106 <monoyen2001@gmail.com>
2024-12-24 17:26:20 +0800

    feat: changed url type for us style

--- update_records/urls.py
+++ update_records/urls.py
@@ -5,8 +5,7 @@ from .views import index,new,show,edit,delete
 app_name = "update_records"
 
 urlpatterns = [
-    path('', index , name = "index" ),
-    path('new/', new , name = "new" ),
+    # 移到projects urls
     path('<int:update_id>', show , name = "show" ),
     path('<int:update_id>/edit', edit , name = "edit" ),
     path('<int:update_id>/delete', delete , name = "delete" ),
