e8e8cc052af31013ae12065d9b7986dae0ff10a0
LAPTOP-0GVE4AVI\Linda <lindaqaq88@gmail.com>
2024-12-24 23:12:54 +0800

    refactor: resolve users conflicts

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,8 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 app_name = "profiles"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("new", views.new, name="new"),
-    path("<int:id>", views.show, name="show"),
-    path("<int:id>/edit", views.edit, name="edit"),
-    path("<int:id>/delete", views.delete, name="delete"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- users/views.py
+++ users/views.py
@@ -5,89 +5,45 @@ from accounts.views import User
 from django.utils import timezone
 
 
-
-def profiles_index(request,id):
+def profiles_index(request, id):
     account = get_object_or_404(User, id=id)
-    profile = get_object_or_404(Profile,account=account)
+    profile = get_object_or_404(Profile, account=account)
     if request.POST:
-        form = ProfileForm(request.POST,request.FILES,instance=profile)
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
             form.save()
             profile.update_at = timezone.now()
             profile.save()
-            return redirect("accounts:profiles_index", id =account.id)
+            return redirect("accounts:profiles_index", id=account.id)
 
-    return render(request, "profiles/index.html", {"account":account, "profile":profile})
-
-
+    return render(
+        request, "profiles/index.html", {"account": account, "profile": profile}
+    )
 
 
 def profiles_new(request, id):
-    account = get_object_or_404(User , id=id)
+    account = get_object_or_404(User, id=id)
     profile = get_object_or_404(Profile, account=account)
     form = ProfileForm(request.FILES, instance=profile)
     return render(
         request,
         "profiles/new.html",
-        {"profile": profile,  "form": form, "account":account},
+        {"profile": profile, "form": form, "account": account},
     )
 
 
-<<<<<<< HEAD
 def profiles_edit(request, id):
-    account = get_object_or_404(User , id=id)
+    account = get_object_or_404(User, id=id)
     profile = get_object_or_404(Profile, account=account)
     format_time = profile.birthday.strftime("%Y-%m-%d")
     form = ProfileForm(request.FILES, instance=profile)
-=======
-def new(request):
-    if request.POST:
-        form = UserForm(request.POST, request.FILES)
-        form.save()
-        return redirect("users:index")
->>>>>>> 589bbb8 (feat:upload function)
     return render(
         request,
         "profiles/edit.html",
-        {"profile": profile,  "form": form, "account":account,"format_time":format_time},
+        {
+            "profile": profile,
+            "form": form,
+            "account": account,
+            "format_time": format_time,
+        },
     )
-
-<<<<<<< HEAD
-=======
-
-def show(request, id):
-    user = get_object_or_404(User, id=id)
-    if request.POST:
-        form = UserForm(request.POST, request.FILES, instance=user)
-        form.save()
-        return redirect("users:index")
-
-    return render(
-        request,
-        "users/show.html",
-        {"user": user},
-    )
-
-
-def edit(request, id):
-    user = get_object_or_404(User, id=id)
-    format_time = user.birthday.strftime("%Y-%m-%d")
-    form = UserForm(request.FILES, instance=user)
-    return render(
-        request,
-        "users/edit.html",
-        {"user": user, "format_time": format_time, "form": form},
-    )
-
-
-def delete(request, id):
-    user = get_object_or_404(User, id=id)
-    if request.POST:
-        user.delete()
-        return redirect("users:index")
-    return render(
-        request,
-        "users/delete.html",
-        {"user": user},
-    )
->>>>>>> 589bbb8 (feat:upload function)
