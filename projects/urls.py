7bd778752be31190c7f9225058ebe93cce6aec66
LAPTOP-0GVE4AVI\Linda <lindaqaq88@gmail.com>
2024-12-24 22:53:34 +0800

    refactor: resolve projects conflicts

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import show, edit, delete, collect_projects
+from .views import show, edit, delete, collect_projects, like_projects
 from comments.views import index as comment_index, new as comment_new
 from faqs.views import index as faq_index, new as faq_new
-from update_records.views import index as update_records_index, new as update_records_new
+from update_records.views import (
+    index as update_records_index,
+    new as update_records_new,
+)
 
 app_name = "projects"
 
@@ -16,6 +19,7 @@ urlpatterns = [
     path("<int:id>/comments/new", comment_new, name="comment_new"),
     path("<int:id>/faq", faq_index, name="faq_index"),
     path("<int:id>/faq/new", faq_new, name="faq_new"),
-    path("<int:id>/update_records", update_records_index , name = "update_records_index" ),
-    path("<int:id>/update_records/new", update_records_new , name = "update_records_new" ),
+    path("<int:id>/update_records", update_records_index, name="update_records_index"),
+    path("<int:id>/update_records/new", update_records_new, name="update_records_new"),
+    path("<int:id>/like", like_projects, name="like"),
 ]
