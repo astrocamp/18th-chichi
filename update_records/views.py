2173f945c1ef2b8467cb61bec4860214e5fac60e
IanYang1106 <monoyen2001@gmail.com>
2024-12-24 17:26:20 +0800

    feat: changed url type for us style

--- update_records/views.py
+++ update_records/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render,redirect,get_object_or_404
 from .forms import UpdateRecordFrom
 from .models import UpdateRecord
 from projects.models import Project
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 
-
+@login_required
 def index(request, id):
     project = get_object_or_404(Project, id=id)
     
@@ -14,7 +15,7 @@ def index(request, id):
             description=request.POST['description'],
             project=project
         )
-        return redirect("update_records:index", id=id)
+        return redirect("projects:update_records_index", id=project.id)
     
     update_records = UpdateRecord.objects.filter(project=project)
     return render(request, "update_records/index.html", {
@@ -28,39 +29,44 @@ def new(request, id):
 
 
 
-def show(request, id, update_id):
-    project = get_object_or_404(Project, id=id)
-    update_record = get_object_or_404(UpdateRecord, id=update_id, project=project)
+def show(request, update_id):
+    update_record = get_object_or_404(UpdateRecord, id=update_id)
+    project_id = update_record.project.id 
     
     if request.POST:
         form = UpdateRecordFrom(request.POST, instance=update_record)
         form.save()
         update_record.update_at = timezone.now()
         update_record.save()
-        return redirect("update_records:show", id=id, update_id=update_record.id)
+        return redirect("update_records:show", update_id=update_record.id)
     
     return render(request, "update_records/show.html", {
         "update_record": update_record,
-        "id": id
+        "id": project_id
     })
 
 
 
 
-def edit(request, id, update_id):
-    project = get_object_or_404(Project, id=id)
-    update_record = get_object_or_404(UpdateRecord, id=update_id, project=project)
-    return render(request, "update_records/edit.html",{"update_record":update_record,"id":id,})
+def edit(request, update_id):
+    update_record = get_object_or_404(UpdateRecord, id=update_id)
+    project_id = update_record.project.id  
+    
+    return render(request, "update_records/edit.html", {
+        "update_record": update_record,
+        "id": project_id
+    })
 
 
-def delete(request, id, update_id):
-    update_record = get_object_or_404(UpdateRecord,id=update_id)
+def delete(request, update_id):
+    update_record = get_object_or_404(UpdateRecord, id=update_id)
+    project_id = update_record.project.id  
+    
     if request.POST:
         update_record.delete()
+        return redirect("projects:update_records_index", id=project_id)  
 
-        return redirect("update_records:index" ,id=id)
-
-    return render(request, "update_records/delete.html",{
+    return render(request, "update_records/delete.html", {
         "update_record": update_record,
-        "id": id        
+        "id": project_id
     })
