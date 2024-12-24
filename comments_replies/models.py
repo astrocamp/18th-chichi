174e5e3592eafd1ee72c4aee1bdfbf4a26c078b3
LAPTOP-0GVE4AVI\Linda <lindaqaq88@gmail.com>
2024-12-24 14:10:09 +0800

    refactor: resolve comments templates conflicts

--- comments_replies/models.py
+++ comments_replies/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class CommentsReplies(models.Model):
     content = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)
-    update_at=models.DateTimeField(null=True)
+    update_at = models.DateTimeField(null=True)
+    account = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comments_replies"
+    )
--- comments_replies/views.py
+++ comments_replies/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import CommentsReplies
 from django.utils import timezone
 
+
 def index(request):
     if request.POST:
         comments_reply = CommentsReplies()
@@ -9,7 +10,8 @@ def index(request):
         comments_reply.save()
         return redirect("comments_replies:index")
 
-    comments_replies = CommentsReplies.objects.all()
+    comments_replies = CommentsReplies.objects.filter(account=request.user)
+    CommentsReplies.objects.create(content="content", account=request.user)
     return render(
         request,
         "comments_replies/index.html",
@@ -24,10 +26,10 @@ def new(request):
 def show(request, id):
     comments_reply = get_object_or_404(CommentsReplies, id=id)
     if request.POST:
-        comments_reply.content=request.POST.get("content")
-        comments_reply.update_at=timezone.now()
+        comments_reply.content = request.POST.get("content")
+        comments_reply.update_at = timezone.now()
         comments_reply.save()
-        return redirect("comments_replies:show",id=comments_reply.id)
+        return redirect("comments_replies:show", id=comments_reply.id)
 
     return render(
         request,
@@ -35,9 +37,14 @@ def show(request, id):
         {"comments_reply": comments_reply},
     )
 
-def edit(request,id):
-    comments_reply=get_object_or_404(CommentsReplies,id=id)
-    return render(request,"comments_replies/edit.html",{"comments_reply":comments_reply},)
+
+def edit(request, id):
+    comments_reply = get_object_or_404(CommentsReplies, id=id)
+    return render(
+        request,
+        "comments_replies/edit.html",
+        {"comments_reply": comments_reply},
+    )
 
 
 def delete(request, id):
