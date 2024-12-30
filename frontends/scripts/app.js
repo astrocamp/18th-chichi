1f986e09bf7db2b037068290feba5cb27db493af
IanYang1106 <monoyen2001@gmail.com>
2024-12-30 18:58:06 +0800

    feat: add Font Awesome for icon usage

--- frontends/scripts/app.js
+++ frontends/scripts/app.js
@@ -5,3 +5,14 @@ window.Alpine = Alpine;
 window.htmx = htmx;
 
 Alpine.start();
+
+// Font Awesome 引入
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faSpinner, faHouse, faHeart } from "@fortawesome/free-solid-svg-icons"; // 引入 spinner 圖標
+import "@fortawesome/fontawesome-svg-core/styles.css"; // 引入基本樣式
+
+// 將圖標添加到庫中
+library.add(faSpinner, faHouse, faHeart);
+
+// 自動掃描 DOM 並渲染圖標
+dom.watch();
