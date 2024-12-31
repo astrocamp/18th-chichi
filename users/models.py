b82061510a078bcba5dd854823118888d551757c
elaine <lindaqaq88@gmail.com>
2024-12-31 16:54:54 +0800

    fix: users profile functions

--- users/models.py
+++ users/models.py
@@ -4,13 +4,13 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     name = models.CharField(max_length=50)
-    birthday = models.DateField(null=True, blank=True)
-    location = models.CharField(max_length=100, null=True, blank=True)
-    website = models.URLField(null=True, blank=True)
-    bio = models.TextField(null=True, blank=True)
-    phone = models.CharField(max_length=50)
+    birthday = models.DateField(null=True, blank=True, default="")
+    location = models.CharField(max_length=100, null=True, blank=True, default="")
+    website = models.URLField(null=True, blank=True, default="")
+    bio = models.TextField(null=True, blank=True, default="")
+    phone = models.CharField(max_length=50, null=True, blank=True, default="")
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(null=True, auto_now=True)
     account = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField(
         upload_to="profile_pictures/", null=True, blank=True
--- users/views.py
+++ users/views.py
@@ -1,19 +1,35 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Profile
 from .forms import ProfileForm
-from django.contrib.auth.decorators import login_required
+from accounts.views import User
+from django.utils import timezone
+from django.contrib import messages
+
 
 @login_required
 def index(request):
     account = request.user
-    profile = get_object_or_404(Profile,account=account)
+    profile = get_object_or_404(Profile, account=account)
     if request.POST:
         form = ProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
             form.save()
-            return redirect("profile:index")
+            messages.success(request, "個人資料更新成功。")
+            profile.updated_at = timezone.now()
+            profile.save()
+            return redirect("accounts:profiles_index", id=account.id)
+        else:
+            messages.error(request, "個人資料新增失敗。")
+            return render(
+                request,
+                "profiles/new.html",
+                {"profile": profile, "form": form, "account": account},
+            )
 
-    return render(request, "profiles/index.html", {"account":account, "profile":profile})
+    return render(
+        request, "profiles/index.html", {"account": account, "profile": profile}
+    )
+
 
 @login_required
 def new(request):
@@ -26,12 +42,18 @@ def new(request):
         {"profile": profile, "form": form, "account": account},
     )
 
+
 @login_required
 def edit(request):
     account = request.user
     profile = get_object_or_404(Profile, account=account)
-    format_time = profile.birthday.strftime("%Y-%m-%d")
-    form = ProfileForm(request.FILES, instance=profile)
+    if profile.birthday:
+        format_time = profile.birthday.strftime("%Y-%m-%d")
+    else:
+        format_time = ""
+
+        form = ProfileForm(request.FILES, instance=profile)
+
     return render(
         request,
         "profiles/edit.html",
