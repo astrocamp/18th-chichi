0f94980d31da4c01369983dcd69acff17bda2a4e
Jason Wang <a27371510@gmail.com>
2024-12-31 11:03:33 +0800

    issues/81 feat: add sortable function in FAQs

--- faqs/models.py
+++ faqs/models.py
@@ -5,5 +5,8 @@ class Faq(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     question = models.CharField(max_length=100)
     answer = models.TextField(null=True)
-    create_at = models.DateTimeField(auto_now_add=True)  
-    update_at = models.DateTimeField(null=True)  
+    created_at = models.DateTimeField(auto_now_add=True)  
+    updated_at = models.DateTimeField(auto_now=True)  
+    position = models.PositiveIntegerField(default=0)
+    class Meta:
+        ordering = ["position"]
--- faqs/urls.py
+++ faqs/urls.py
@@ -4,9 +4,10 @@ from . import views
 app_name = "faqs"
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('new/', views.new, name="new"),
-    path('<int:id>', views.show, name="show"),
-    path('<int:id>/edit', views.edit, name="edit"),
-    path('<int:id>/delete', views.delete, name="delete"),
+    path("", views.index, name="index"),
+    path("new/", views.new, name="new"),
+    path("<int:id>", views.show, name="show"),
+    path("<int:id>/edit", views.edit, name="edit"),
+    path("<int:id>/delete", views.delete, name="delete"),
+    path("updated-faq-position/", views.updated_faq_position, name="updated_faq_position"),
 ]
