ed627c860fcb1b9a7e6339d9ecb06cedee331b9e
IanYang1106 <monoyen2001@gmail.com>
2024-12-31 22:59:07 +0800

    feat: created homepages and update flash, navbar

--- frontends/scripts/app.js
+++ frontends/scripts/app.js
@@ -46,11 +46,26 @@ Alpine.start();
 
 // Font Awesome 引入
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { faSpinner, faHouse, faHeart, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"; // 引入 spinner 圖標
+import {
+  faSpinner,
+  faHouse,
+  faHeart,
+  faMagnifyingGlass,
+  faHandshake,
+  faShieldAlt,
+  faUsers,
+} from "@fortawesome/free-solid-svg-icons"; // 引入 spinner 圖標
 import "@fortawesome/fontawesome-svg-core/styles.css"; // 引入基本樣式
 
-// 將圖標添加到庫中
-library.add(faSpinner, faHouse, faHeart, faMagnifyingGlass);
+library.add(
+  faSpinner,
+  faHouse,
+  faHeart,
+  faHandshake,
+  faShieldAlt,
+  faUsers,
+  faMagnifyingGlass
+);
 
 // 自動掃描 DOM 並渲染圖標
 dom.watch();
