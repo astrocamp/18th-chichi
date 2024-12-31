2943c8ae6f0b53b7d2f2d2b73e30254bcd785c83
陳致宇 <chenchihyu@MacBook-Air.local>
2024-12-31 14:15:24 +0800

    fix:Fix issue where Google login does not automatically create a user profile

--- users/apps.py
+++ users/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self):
+        import users.signals  # 替換成你的 app 名稱
