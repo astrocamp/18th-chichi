1953b555259a1e3bc5102d231a8e6c676f2683b9
Jason Wang <a27371510@gmail.com>
2025-01-01 04:06:39 +0800

    fix: duplicated migration# with slug id

--- faqs/migrations/0004_alter_faq_options_rename_create_at_faq_created_at_and_more.py
+++ faqs/migrations/0004_alter_faq_options_rename_create_at_faq_created_at_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('faqs', '0002_faq_project'),
+        ('faqs', '0003_faq_slug'),
     ]
 
     operations = [
