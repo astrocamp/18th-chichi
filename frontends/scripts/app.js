0688d2f6bef47bb3cddc4c4ddebfc544b665726f
IanYang1106 <monoyen2001@gmail.com>
2024-12-31 14:22:02 +0800

    refactor: replace custom SVG with Font Awesome icons

--- frontends/scripts/app.js
+++ frontends/scripts/app.js
@@ -8,11 +8,17 @@ Alpine.start();
 
 // Font Awesome 引入
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { faSpinner, faHouse, faHeart } from "@fortawesome/free-solid-svg-icons"; // 引入 spinner 圖標
-import "@fortawesome/fontawesome-svg-core/styles.css"; // 引入基本樣式
+import {
+  faSpinner,
+  faHouse,
+  faHeart,
+  faArrowRight,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 // 將圖標添加到庫中
-library.add(faSpinner, faHouse, faHeart);
+library.add(faSpinner, faHouse, faHeart, faArrowRight, faCheck);
 
 // 自動掃描 DOM 並渲染圖標
 dom.watch();
