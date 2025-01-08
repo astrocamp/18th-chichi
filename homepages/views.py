c4d66e5c81b78984403a47d5d03a2c518ab966f7
lioouzzz <lioou.work@gmail.com>
2025-01-08 20:48:42 +0800

    style : render category card

--- homepages/views.py
+++ homepages/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from projects.models import Project
 from django.core.paginator import Paginator
+from categories.models import Category
 
 
 def homepages(request):
     projects = Project.objects.all().order_by("-created_at")
     paginator = Paginator(projects, 12)
-
+    categories = Category.objects.filter(parent=None)
     page = request.GET.get("page")
     projects = paginator.get_page(page)
-    return render(request, "homepages/homepages.html", {"projects": projects})
+    return render(
+        request,
+        "homepages/homepages.html",
+        {"projects": projects, "categories": categories},
+    )
